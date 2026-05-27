bee363e946af69994453e520e7b8001879a6b37c
Dimitrios Giannopoulos <giannopoulos21996@gmail.com>
2026-05-27 23:13:21 +0300

    feat: add request logger middleware for tracking request durations and methods

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import cors from "cors";
 import callRoutes from "./routes/callRoutes.js"
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import { requestLogger } from "./middleware/requestLogger.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 
 app.use("/calls", callRoutes);
 
